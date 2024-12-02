9b89d36251c96102dcf16b0365001c5faa2c74a9
canit0221 <rladydtn78@gmail.com>
2024-12-02 17:23:04 +0900

    UPDATE

--- 클래스와 함수 사용하기/class.py
+++ 클래스와 함수 사용하기/class.py
@@ -1,12 +1,23 @@
 class Person:  # Person 클래스 생성
     def __init__(self):  # 클래스 초기 실행 함수
         self.name = input("이름: ")  # 이름 입력 및 저장
-        self.age = int(input("나이: "))  # 나이 입력 후 정수 변환
         
-        while True:  # 반복문 시작
+        # 나이 유효성 검사 반복문 시작
+        while True:
+            try: # 숫자만 입력받기
+                age = int(input("나이: ")) # 나이 입력 받고 정수 변환
+                if age > 0:  # 나이가 0보다 크면
+                    self.age = age # 나이 저장
+                    break # 반복문 종료
+                else: # 나이가 0보다 작으면
+                    print("유효하지 않은 나이입니다.") # 안내
+            except ValueError: # 숫자 외 입력 예외 처리
+                print("숫자만 입력해주세요.") # 
+        
+        while True:  # 성별 유효성 검사 반복문 시작
             gender = input("성별: ").lower()  # 성별 입력 후 소문자 변환
             if gender in ['male', 'female']:  # male 또는 female 입력 검사
-                self.gender = '남성' if gender == 'male' else '여성'  # 영문 성별의 한글 변환
+                self.gender = gender  # 영문 성별의 한글 변환
                 break  # 반복문 종료
             print("잘못된 성별을 입력하셨습니다. 'male' 또는 'female'을 입력하세요.")  # 잘못된 입력 메시지 출력
 
@@ -14,10 +25,12 @@ class Person:  # Person 클래스 생성
         print(f"이름: {self.name}, 성별: {self.gender}\n나이: {self.age}")  # 저장된 정보 출력
     
     def greet(self):  # 인사 기능 함수
-        if self.age >= 20:  # 성인 여부 확인
-            print(f"안녕하세요, {self.name}! 성인이시군요!")  # 성인 인사말 출력
-        else:  # 미성년자 조건
-            print(f"안녕하세요, {self.name}! 미성년자시군요!")  # 미성년자 인사말 출력
+        if self.age < 14:  # 어린이 조건
+            print(f"안녕하세요, {self.name} 어린이!")  # 어린이 인사말 출력 
+        elif self.age < 20:  # 청소년 조건
+            print(f"안녕하세요, {self.name} 학생!")  # 청소년 인사말 출력
+        else:  # 성인 여부 확인
+            print(f"안녕하세요, {self.name}님!")  # 성인 인사말 출력
 
 p1 = Person()  # Person 클래스 객체 생성
 p1.display()  # 객체 정보 출력

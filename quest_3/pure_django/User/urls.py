62d7ad61e37f695c4094836b2735780f4daac562
canit0221 <rladydtn78@gmail.com>
2025-01-13 21:26:22 +0900

    urls

--- quest_3/pure_django/User/urls.py
+++ quest_3/pure_django/User/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'User'
+
 urlpatterns = [
     # path('signup/', views.signup, name='signup'),
-    # path('login/', views.login, name='login'),
+    path('login/', views.login, name='login'),
     # path('logout/', views.logout, name='logout'),
     # path('user_profile', views.user_profile, name='user_profile'),
 ]
